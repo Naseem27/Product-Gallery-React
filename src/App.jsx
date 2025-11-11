742205b0b3b220c046d8bc47600b8c66e85fe0b1
Naseem27 <naseem.n2003@gmail.com>
2025-11-11 19:36:43 +0530

    Initial project commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import "./App.css";
 import ProductList from "./components/ProductList";
 import ProductDetails from "./components/ProductDetails";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="head">
-          <h1>Products Gallery</h1>
-        </div>
-
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-        </Routes>
+    <div className="App">
+      <div className="head">
+        <h1>Products Gallery</h1>
       </div>
-    </Router>
+
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="*" element={<ProductList />} />
+      </Routes>
+    </div >
   );
 }
 
